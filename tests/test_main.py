d0f3937ca71088740ad38fcf69228fed5102b0cf
VikasYadav2005 <vikas.cy@somaiya.edu>
2025-04-10 11:23:09 +0530

    test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,8 @@
-def test_sample():
+import unittest
+
+class SampleTest(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
